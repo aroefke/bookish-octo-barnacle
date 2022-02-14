b96b560e0701bc79f70bd8f9553486e0d6f6d958
aroefke <anton.roefke@outlook.de>
2022-02-14 15:57:46 +0100

    continuous api request func

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import pymongo
 import requests
 from pathlib import Path
 from pprint import pprint
+from time import sleep
 
 # save your api.weatherapi.com key in 'api-key' file
 APIKEY = Path('api-key').read_text()
@@ -13,16 +14,20 @@ db = client.weather_db
 weather_data = db.weather_data
 
 def main():
+	
 	locations = ['London', 'Berlin', 'Paris']
-	for loc in locations:	
-		r = requests.get(
-			f'https://api.weatherapi.com/v1/current.json?key={APIKEY}&q={loc}&aqi=no'
-		)
-		json = r.json()
-		#pprint(json)
-		print(json['location']['name'], json['current']['temp_c'], 'Celcius')
-		oid = weather_data.insert_one(json).inserted_id
-		print(oid)
+	
+	while True:
+		for loc in locations:	
+			r = requests.get(
+				f'https://api.weatherapi.com/v1/current.json?key={APIKEY}&q={loc}&aqi=no'
+			)
+			json = r.json()
+			print(json['location']['name'], json['current']['temp_c'], 'Celcius')
+			oid = weather_data.insert_one(json).inserted_id
+			print(oid)
+		print("sleeping 1min")
+		sleep(60)
 
 
 if __name__ == "__main__":
